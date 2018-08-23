f67ba90c94092b60d3d70d55f98a3ce820a1a04b
Logon Aluno <logonrmlocal@fiap.com.br>
2018-08-22 22:25:17 -0300

    aula jquery e objetos em javascript

---
+++ jquery/js/meujs.js
@@ -0,0 +1,12 @@
+$(function () {
+
+    $('button').click(function () {
+
+        $('p').hide();
+
+
+        $('h1').css({ "font-family": "arial", "color": "red" });
+
+        $('#caixa').css({ 'width': '200px', 'height': '200px', 'border': '1px solid red', "background": '#ccc' })
+    })
+});
